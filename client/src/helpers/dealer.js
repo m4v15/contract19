6520bc9cf0e966c403713a624ac96d4f59aa5e81
Matt Davis <matthewpeterdavis@gmail.com>
2020-05-22 15:10:20 +0100

    deal cards slightly differently

--- client/src/helpers/dealer.js
+++ client/src/helpers/dealer.js
@@ -2,12 +2,12 @@ import Card from "./card";
 
 export default class Dealer {
   constructor(scene) {
-    this.dealCards = hand => {
+    this.dealCards = (hand) => {
       console.log(hand);
       hand.forEach((card, i) => {
         console.log(card);
         let playerCard = new Card(scene);
-        playerCard.render(475 + i * 100, 650, card);
+        playerCard.render(175 + i * 75, 650, card);
       });
     };
   }
